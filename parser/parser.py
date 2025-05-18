593172c820203a5e8aab4cb915a9ddab895fd019
nack098 <nack098gamer@gmail.com>
2025-05-18 16:25:42 +0700

    feat(parser): add parser

---
+++ parser/parser.py
@@ -0,0 +1,62 @@
+from collections.abc import Iterable
+from collections import deque
+
+
+class Parser:
+
+    token: Iterable
+    stack: deque[str] = []
+    rules: dict[int, list[str]]
+    parsing_table: dict[tuple[str, str], int]
+    allow_any: list[str]
+    empty_symbols: str
+    starting_symbol: int
+    current_symbol: str = ""
+
+    def __init__(
+            self,
+            rules: dict[int, list[str]],
+            parsing_table: dict[tuple[str, str], int],
+            allow_any: Iterable = [],
+            empty_symbols="e",
+            starting_symbol:int = 0):
+        self.rules = rules
+        self.parsing_table = parsing_table
+        self.starting_symbol = starting_symbol
+        self.empty_symbols = empty_symbols
+        self.allow_any = set(allow_any)
+    
+    def __iter__(self):
+        return self
+    
+    def _parse(self, front):
+        rule_idx = self.parsing_table.get((front, self.current_symbol))
+        if not rule_idx:
+            raise ValueError(f"Unknown rule for ({front}, {self.current_symbol})")
+        self.stack.extend(reversed(self.rules[rule_idx]))
+
+    def __next__(self):
+        try:
+            front = self.stack.pop()
+            if (front == self.empty_symbols):
+                return front
+            if (self.current_symbol == ""):
+                self.current_symbol = next(self.token)
+            if (front in self.allow_any):
+                self.stack.append(self.current_symbol)
+                return front
+            if (front == self.current_symbol):
+                self.current_symbol = ""
+                return front
+            else:
+                self._parse(front)
+                return front
+        except StopIteration:
+            self._parse(front)
+            return front
+        except IndexError:
+            raise StopIteration
+    
+    def set(self, token: Iterable):
+        self.token = token
+        self.stack.extend(self.rules[self.starting_symbol])
---
+++ tests/test_parser.py
@@ -0,0 +1,84 @@
+from tokenizer import Tokenizer
+from parser import Parser
+
+
+skip_symbols = {" ", "\n"}
+rules = {
+    0: ["asm"],
+    1: ["op", "oprnd", "asm"],
+    2: ["e"],
+    3: ["CLRA"],
+    4: ["MOV"],
+    5: ["ADD"],
+    6: ["MOVA"],
+    7: ["reg", "param"],
+    8: ["e"],
+    9: [",", "num"],
+    10: ["e"],
+    11: ["R1"],
+    12: ["R2"],
+    13: ["100"],
+    14: ["200"],
+}
+parsing_table = {
+    ("asm", "CLRA"): 1,
+    ("asm", "MOV"): 1,
+    ("asm", "ADD"): 1,
+    ("asm", "MOVA"): 1,
+    ("asm", ""): 2,
+    ("op", "CLRA"): 3,
+    ("op", "MOV"): 4,
+    ("op", "ADD"): 5,
+    ("op", "MOVA"): 6,
+    ("oprnd", "R1"): 7,
+    ("oprnd", "R2"): 7,
+    ("oprnd", "CLRA"): 8,
+    ("oprnd", "MOV"): 8,
+    ("oprnd", "ADD"): 8,
+    ("oprnd", "MOVA"): 8,
+    ("reg", "R1"): 11,
+    ("reg", "R2"): 12,
+    ("param", ","): 9,
+    ("param", "CLRA"): 10,
+    ("param", "MOV"): 10,
+    ("param", "ADD"): 10,
+    ("param", "MOVA"): 10,
+    ("param", ""): 10,
+    ("num", "100"): 13,
+    ("num", "200"): 14,
+}
+
+
+tokenizer = Tokenizer(skip_symbols=skip_symbols)
+parser = Parser(rules=rules, parsing_table=parsing_table)
+
+
+def test_parser_basic():
+    text = "MOV R1,200"
+    tokenizer.set(text)
+    parser.set(token=tokenizer)
+    res = list(parser)
+    expect = [
+        "asm", "op", "MOV", "oprnd", "reg", "R1", "param", ",", "num", "200", "asm", "e"]
+    assert res == expect
+
+def test_parser_multiline():
+    text = """
+        CLRA
+        MOV R1,100
+        MOV R2,200
+        ADD R1
+        ADD R2
+        MOVA R1
+    """
+    tokenizer.set(text)
+    parser.set(token=tokenizer)
+    res = list(parser)
+    expect = [
+        "asm","op", "CLRA", "oprnd", "e",
+        "asm", "op", "MOV", "oprnd", "reg", "R1", "param", ",", "num", "100",
+        "asm", "op", "MOV", "oprnd", "reg", "R2", "param", ",", "num", "200",
+        "asm", "op", "ADD", "oprnd", "reg", "R1", "param", "e",
+        "asm", "op", "ADD", "oprnd", "reg", "R2", "param", "e",
+        "asm", "op", "MOVA", "oprnd", "reg", "R1", "param", "e", "asm", "e"]
+    assert res == expect
