895beb22bd0d8e29220ed3069859515851858e53
nack098 <nack098gamer@gmail.com>
2025-05-19 02:39:21 +0700

    feat(tokenizer): make tokenizer using iterator

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -5,8 +5,6 @@ from .estate import EState
 class Tokenizer:
 
     raw: Iterable = []
-    index: int = 0
-    length: int = 0
     state: EState = EState.SKIP
     skip_symbols: list[str]
 
@@ -17,29 +15,32 @@ class Tokenizer:
     
     def _state_machine(self):
         buf = ""
-        while self.index < self.length:
-            curr = self.raw[self.index]
-            match self.state:
-                case EState.SKIP:
-                    if curr not in self.skip_symbols:
+        try:
+            if self.state != EState.COLLON:
+                curr = next(self.raw)
+            while True:
+                match self.state:
+                    case EState.SKIP:
+                        if curr not in self.skip_symbols:
+                            self.state = EState.TEXT
+                            continue
+                        curr = next(self.raw)
+                    case EState.TEXT:
+                        if curr == ",":
+                            self.state = EState.COLLON
+                            return buf
+                        if curr in self.skip_symbols:
+                            self.state = EState.SKIP 
+                            return buf
+                        buf += curr
+                        curr = next(self.raw)
+                    case EState.COLLON:
                         self.state = EState.TEXT
-                        continue
-                    self.index += 1
-                case EState.TEXT:
-                    if curr == ",":
-                        self.state = EState.COLLON
-                        return buf
-                    if curr in self.skip_symbols:
-                        self.state = EState.SKIP 
-                        return buf
-                    buf += curr
-                    self.index += 1
-                case EState.COLLON:
-                    self.state = EState.TEXT
-                    self.index += 1
-                    return ","
-        if buf:
-            return buf
+                        return ","
+        except StopIteration:
+            if buf:
+                return buf
+            raise StopIteration
     
     def __iter__(self):
         return self
@@ -53,7 +54,5 @@ class Tokenizer:
     
     def __call__(self, text:Iterable):
         self.state = EState.SKIP
-        self.length = len(text)
-        self.index = 0
         self.raw = text
         return self
